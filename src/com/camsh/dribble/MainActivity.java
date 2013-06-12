15969291acc62e4688cfa4a345126fab95ac6e7e
Cameron Deans <CDeansy@gmail.com>
2013-06-12 01:10:12 +0100

    Implemented the cards library and tested using the default cards.

--- src/com/camsh/dribble/MainActivity.java
+++ src/com/camsh/dribble/MainActivity.java
@@ -1,8 +1,15 @@
 package com.camsh.dribble;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+
+import com.fima.cardsui.objects.CardStack;
+import com.fima.cardsui.views.CardUI;
+
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +19,17 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		new API.getSubreddit().execute("S");
+
+        CardUI mCardView = (CardUI) findViewById(R.id.cardsview);
+        mCardView.setSwipeable(true);
+
+        // add one card
+        mCardView.addCard(new MyImageCard("Nexus 4 Part 1",R.drawable.url1));
+        mCardView.addCardToLastStack(new MyImageCard("Nexus 4 Part 2",R.drawable.url2));
+        mCardView.addCardToLastStack(new MyImageCard("Nexus 4 Part 3", R.drawable.url3));
+
+        // draw cards
+        mCardView.refresh();
 		
 	}
 
