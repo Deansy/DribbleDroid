f01f7b5641d7b3f067125d9ca88ae3df4f0236a4
Cameron Deans <CDeansy@gmail.com>
2013-06-12 01:22:48 +0100

    Begginings of changing the default cards into what I want from them.

--- src/com/camsh/dribble/ImageCard.java
+++ src/com/camsh/dribble/ImageCard.java
@@ -3,22 +3,23 @@ package com.camsh.dribble;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fima.cardsui.objects.Card;
 
-public class MyCard extends Card {
+public class ImageCard extends Card {
 
-    public MyCard(String title){
-        super(title);
+    public ImageCard(String title, int image){
+        super(title, image);
     }
 
     @Override
     public View getCardContent(Context context) {
-        View view = LayoutInflater.from(context).inflate(R.layout.card_ex, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.card_picture, null);
 
         ((TextView) view.findViewById(R.id.title)).setText(title);
-
+        ((ImageView) view.findViewById(R.id.imageView1)).setImageResource(image);
 
         return view;
     }
--- src/com/camsh/dribble/MainActivity.java
+++ src/com/camsh/dribble/MainActivity.java
@@ -1,15 +1,10 @@
 package com.camsh.dribble;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
 
-import com.fima.cardsui.objects.CardStack;
 import com.fima.cardsui.views.CardUI;
-
-import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -24,9 +19,11 @@ public class MainActivity extends Activity {
         mCardView.setSwipeable(true);
 
         // add one card
-        mCardView.addCard(new MyImageCard("Nexus 4 Part 1",R.drawable.url1));
-        mCardView.addCardToLastStack(new MyImageCard("Nexus 4 Part 2",R.drawable.url2));
-        mCardView.addCardToLastStack(new MyImageCard("Nexus 4 Part 3", R.drawable.url3));
+        mCardView.addCard(new ImageCard("Nexus 4 Part 1",R.drawable.url1));
+
+        mCardView.addCard(new ImageCard("Nexus 4 Part 2", R.drawable.url2));
+
+        mCardView.addCard(new ImageCard("Nexus 4 Part 3", R.drawable.url3));
 
         // draw cards
         mCardView.refresh();
